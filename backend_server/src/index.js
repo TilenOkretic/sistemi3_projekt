00625398a22203d60b159d74b788aa82f9a694e5
TilenOkretic <tilen.okretic@gmail.com>
2022-08-13 20:55:10 +0200

    :sparkles: :tada: backend test

--- backend_server/src/index.js
+++ backend_server/src/index.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 9999;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${require('os').networkInterfaces().eth1[0].address}:${PORT}`);
+    console.log(`Server is running on port http://${require('os').networkInterfaces().eth1[0].address}:${PORT}`);
 });
